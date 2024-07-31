eb1232b9a2fec5250eeedea9c96a9cfc33469767
vincecoscia <xvincentcosciax@gmail.com>
2024-07-31 17:25:09 -0400

    Added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Open_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 
 const opensans = Open_Sans({ subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={opensans.className} >
       <Component {...pageProps} />
+      <Analytics />
     </main>
   );
 }
