7ba7a7ed59aef96f4e568843ecb10a16dd7a0f28
vincecoscia <xvincentcosciax@gmail.com>
2024-07-31 17:34:23 -0400

    Added favicon fixed errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,39 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Open_Sans } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 const opensans = Open_Sans({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={opensans.className} >
-      <Component {...pageProps} />
-      <Analytics />
-    </main>
+    <>
+      <Head>
+        <title>Green Go Lawn Care</title>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+      </Head>
+      <main className={opensans.className}>
+        <Component {...pageProps} />
+        <Analytics />
+      </main>
+    </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-        <title>Green Go Lawn Care</title>
       </Head>
       <body>
         <Main />
